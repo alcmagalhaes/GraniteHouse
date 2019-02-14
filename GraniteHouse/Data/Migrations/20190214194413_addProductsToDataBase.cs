8524932aa9c0c10628d5c07582f0491ff393e06a
André Magalhães <amagalhaes@triplait.com>
2019-02-14 17:45:33 -0200

    Criacao da entidade Products

---
+++ GraniteHouse/Data/Migrations/20190214194413_addProductsToDataBase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GraniteHouse.Data.Migrations
+{
+    public partial class addProductsToDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    Avaiable = table.Column<bool>(nullable: false),
+                    Image = table.Column<string>(nullable: true),
+                    ShadeColor = table.Column<string>(nullable: true),
+                    ProductTypesId = table.Column<int>(nullable: false),
+                    SpecialTagsId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_ProductTypes_ProductTypesId",
+                        column: x => x.ProductTypesId,
+                        principalTable: "ProductTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_SpecialTags_SpecialTagsId",
+                        column: x => x.SpecialTagsId,
+                        principalTable: "SpecialTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductTypesId",
+                table: "Products",
+                column: "ProductTypesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SpecialTagsId",
+                table: "Products",
+                column: "SpecialTagsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ GraniteHouse/Models/Products.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public bool Avaiable { get; set; }
+
+        public string Image { get; set; }
+
+        public string ShadeColor { get; set; }
+
+        [Display(Name="Product Type")]
+        public int ProductTypesId { get; set; }
+
+        [ForeignKey("ProductTypesId")]
+        public virtual ProductTypes  ProductTypes { get; set; }
+
+        [Display(Name="Special Tag")]
+        public int SpecialTagsId { get; set; }
+
+        [ForeignKey("SpecialTagsId")]
+        public virtual SpecialTags SpecialTags { get; set; }
+
+    }
+}

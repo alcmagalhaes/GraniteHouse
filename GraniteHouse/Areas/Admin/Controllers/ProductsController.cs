611ac2313bcc541ee498cfa7c29fe27a79b13c1c
André Magalhães <amagalhaes@triplait.com>
2019-02-15 10:47:22 -0200

    Products - Create Action Methods (GET, POST)

--- GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
 using GraniteHouse.Models;
 using GraniteHouse.Models.ViewModels;
+using GraniteHouse.Utility;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,13 +18,15 @@ namespace GraniteHouse.Areas.Admin.Controllers
     {
 
         private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _henv;
 
         [BindProperty]
         public ProductsViewModel ProductsVM { get; set; }
 
-        public ProductsController(ApplicationDbContext db)
+        public ProductsController(ApplicationDbContext db, HostingEnvironment henv)
         {
             _db = db;
+            _henv = henv;
 
             ProductsVM = new ProductsViewModel()
             {
@@ -37,5 +42,59 @@ namespace GraniteHouse.Areas.Admin.Controllers
             return View(await products.ToListAsync());
         }
 
+        //GET : Products Create
+        public IActionResult Create()
+        {
+            return View(ProductsVM);
+        }
+
+        //POST : Products Create
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ProductsVM);
+            }
+
+            _db.Products.Add(ProductsVM.Products);
+            await _db.SaveChangesAsync();
+
+            //Image being saved
+            string webRootPath = _henv.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var productsFromDb = _db.Products.Find(ProductsVM.Products.Id);
+
+            if (files.Count != 0)
+            {
+                //Image has been upload
+                var uploads = Path.Combine(webRootPath, SD.ImagesFolder);
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var fileStream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+
+                productsFromDb.Image = @"\" + SD.ImagesFolder + @"\" + ProductsVM.Products.Id + extension;
+
+            }
+            else
+            {
+                //When user does not upload image
+                var uploads = Path.Combine(webRootPath, SD.ImagesFolder + @"\" + SD.DefaultProductImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\" + SD.ImagesFolder + @"\" + ProductsVM.Products.Id + ".png");
+                productsFromDb.Image = @"\" + SD.ImagesFolder + @"\" + ProductsVM.Products.Id + ".png";
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+        }
+
     }
+
 }

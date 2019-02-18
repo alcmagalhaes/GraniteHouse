21b2e061afee8809004c43b163c0595e5d7bc95c
André Magalhães <amagalhaes@triplait.com>
2019-02-18 13:33:26 -0300

    Adicao de Session e SessionExtension

--- GraniteHouse/Startup.cs
+++ GraniteHouse/Startup.cs
@@ -41,6 +41,12 @@ namespace GraniteHouse
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,8 +66,8 @@ namespace GraniteHouse
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
             app.UseAuthentication();
+            app.UseSession();
 
             app.UseMvc(routes =>
             {

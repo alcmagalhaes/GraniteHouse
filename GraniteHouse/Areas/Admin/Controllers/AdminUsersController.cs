b10a391aed84220cc0541c9dfbad26f0789f6afc
André Magalhães <amagalhaes@triplait.com>
2019-02-19 17:15:36 -0300

    User - Index and Edit Action Methods and Views

---
+++ GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminUsersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AdminUsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        //GET : AdminUsers Index
+        public IActionResult Index()
+        {
+            return View(_db.ApplicationUser.ToList());
+        }
+
+        //GET: AdminUsers Edit
+        public async Task<IActionResult> Edit(string id)
+        {
+            if(id == null || id.Trim().Length == 0)
+            {
+                return NotFound();
+            }
+
+            var userFromDb = await _db.ApplicationUser.FindAsync(id);
+            if (userFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFromDb);
+        }
+
+        //POST: AdminUsers Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string id, ApplicationUser applicationUser)
+        {
+            if (id != applicationUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                ApplicationUser userFromDb = _db.ApplicationUser.Where(u => u.Id == id).FirstOrDefault();
+                userFromDb.Name = applicationUser.Name;
+                userFromDb.PhoneNumber = applicationUser.PhoneNumber;
+
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(applicationUser);
+        }
+    }
+}

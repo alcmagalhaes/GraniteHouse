f66dff2a2687a03dfca4b2a6312f827fae7a5001
André Magalhães <amagalhaes@triplait.com>
2019-02-20 16:51:42 -0300

    Appointments - Details View Model

---
+++ GraniteHouse/Models/ViewModels/AppointmentsDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models.ViewModels
+{
+    public class AppointmentsDetailsViewModel
+    {
+        public Appointments Appointments { get; set; }
+        public List<ApplicationUser> SalesPersons { get; set; }
+        public List<Products> Products { get; set; }
+    }
+}

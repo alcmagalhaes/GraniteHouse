1deb19916edbd5dd1de7526130b3a67eb3fe5191
André Magalhães <amagalhaes@triplait.com>
2019-02-20 10:54:22 -0300

    Appoitnments - Index GET Action Method

--- GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models.ViewModels;
 using GraniteHouse.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraniteHouse.Areas.Admin.Controllers
 {
@@ -12,11 +16,32 @@ namespace GraniteHouse.Areas.Admin.Controllers
     [Area("Admin")]
     public class AppointmentsController : Controller
     {
-        
+        private readonly ApplicationDbContext _db;
 
-        public IActionResult Index()
+        public AppointmentsController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        //GET : 
+        public async Task<IActionResult> Index()
+        {
+            ClaimsPrincipal currentUser = this.User;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            AppointmentsViewModel appointmentVM = new AppointmentsViewModel()
+            {
+                Appointments = new List<Models.Appointments>()
+            };
+
+            appointmentVM.Appointments = await _db.Appointments.Include(a => a.SalesPerson).ToListAsync();
+            if (User.IsInRole(SD.AdminEndUser))
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.SalesPersonId == claim.Value).ToList();
+            }
+
+            return View(appointmentVM);
         }
     }
 }
---
+++ GraniteHouse/Models/ViewModels/AppointmentsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models.ViewModels
+{
+    public class AppointmentsViewModel
+    {
+        public List<Appointments> Appointments { get; set; }
+    }
+}

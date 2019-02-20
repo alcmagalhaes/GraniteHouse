babce289c4d67ea726aa9c12b2a447e57496fb3b
André Magalhães <amagalhaes@triplait.com>
2019-02-20 17:50:05 -0300

    Appointments - Edit GET Action Method

--- GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using GraniteHouse.Models.ViewModels;
 using GraniteHouse.Utility;
 using Microsoft.AspNetCore.Authorization;
@@ -68,5 +69,30 @@ namespace GraniteHouse.Areas.Admin.Controllers
 
             return View(appointmentVM);
         }
+
+        //GET : Edit 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = (IEnumerable<Products>)(from p in _db.Products
+                                                   join a in _db.ProductsSelectedForAppointments
+                                                   on p.Id equals a.ProductId
+                                                   where a.AppointmentId == id
+                                                   select p).Include("ProductTypes");
+
+            AppointmentsDetailsViewModel appointmentsDetailsVM = new AppointmentsDetailsViewModel()
+            {
+                Appointments = _db.Appointments.Include(a => a.SalesPerson).Where(a => a.Id == id).FirstOrDefault(),
+                SalesPersons = _db.ApplicationUser.ToList(),
+                Products = products.ToList()
+            };
+
+            return View(appointmentsDetailsVM);
+        }
+
     }
 }
